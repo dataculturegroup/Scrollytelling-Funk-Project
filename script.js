de72c6acb268b10c75b49119d933d23a2cd1b759
Rahul Bhargava <rahulbot@gmail.com>
2023-03-07 18:25:40 -0500

    formatting

--- script.js
+++ script.js
@@ -10,36 +10,32 @@ var scroller = scrollama();
 
 // scrollama event handlers
 function handleStepEnter(response) {
-// response = { element, direction, index }
-var el = response.element;
-
-// remove is-active from all steps
-// then add is-active to this step
-steps.forEach((step) => step.classList.remove("is-active"));
-el.classList.add("is-active");
-
-// update graphic based on step
-document.getElementById("mainImage").className = `${el.dataset.step}`;
+    // response = { element, direction, index }
+    var el = response.element;
+    // remove is-active from all steps
+    // then add is-active to this step
+    steps.forEach((step) => step.classList.remove("is-active"));
+    el.classList.add("is-active");
+    // update graphic based on step
+    document.getElementById("mainImage").className = `${el.dataset.step}`;
 }
 
 function init() {
-scroller
-    .setup({
-    step: "#scrolly article .step",
-    offset: 0.66,
-    debug: false,
-    })
-    .onStepEnter(handleStepEnter);
+    scroller.setup({
+        step: "#scrolly article .step",
+        offset: 0.66,
+        debug: false})
+        .onStepEnter(handleStepEnter);
 
-// setup resize event
-window.addEventListener("resize", scroller.resize);
+    // setup resize event
+    window.addEventListener("resize", scroller.resize);
 }
 
 // kick things off
 init();
 
 function pageLoaded() {
-document.querySelector("h1").style.opacity = 1;
+    document.querySelector("h1").style.opacity = 1;
 }
 window.onload = pageLoaded;
 
